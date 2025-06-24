7a55262e8725e42cc64c5b1bfa1f138ec908eb02
Shubham Agarwal <34504568+shubadmin@users.noreply.github.com>
2025-06-24 17:15:35 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, SRE/DevOps World! This is CI/CD Demo on AWS EC2 🚀"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
