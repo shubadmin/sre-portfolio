000a513059afaa925c61c3d35fef947073d7cbcf
Shubham Agarwal <34504568+shubadmin@users.noreply.github.com>
2025-06-27 14:29:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-@@ -1,10 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, SRE/DevOps World! This is CI/CD Demo on AWS EC2 🚀"
     return "🚀 Hello from Flask via NGINX!"
 
 @app.route("/health")
@@ -13,5 +10,4 @@ def health():
     return {"status": "ok"}, 200
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
     app.run()
